5984f5b4ca8283fd167ebf82747daedd3794f2e8
Juan C. Estevez <juanky.software@gmail.com>
2020-05-11 21:43:37 -0500

    Version 3.0 - Version final. Se incluye efectos en los botones e ingreso de datos por teclado

--- js/app.js
+++ js/app.js
@@ -85,7 +85,7 @@ class Calculadora{
 c = new Calculadora(document.getElementById("display"));
 
 // --- FUNCIONES
-function clickAgregarNumero(){
+function click_agregar_numero(){
   var estado_anterior = display.innerHTML;
   if (c.comprobar_decimales(estado_anterior)) {
     if (estado_anterior == "0") {
@@ -183,36 +183,184 @@ function tecla_igual() {
   }
 }
 
+function tecla_raton_ingresa() {
+  event.target.style.opacity = "0.9";
+}
+
+function tecla_raton_sale() {
+  event.target.style.opacity = "1";
+}
+
+function tecla_presionar() {
+  var tecla = event.target;
+  var ancho = tecla.width
+  console.log(ancho)
+  if (tecla.id == "mas") {
+    tecla.style.width = (ancho-1)+"px";
+  } else {
+    tecla.style.width = (ancho-2)+"px"; 
+  }
+  console.log(tecla.width)
+  setTimeout(function() {
+    tecla.style.width = ancho+"px";
+    console.log(tecla.width)
+  }, 100)
+}
+
+function ingresar_tecla(x){
+  switch (x.code.toString()) {
+    case "Numpad1":
+      ingreso_por_teclado(1);
+      break;
+    case "Digit1":
+      ingreso_por_teclado(1);
+      break;
+    case "Numpad2":
+      ingreso_por_teclado(2);
+      break;
+    case "Digit2":
+      ingreso_por_teclado(2);
+      break;
+    case "Numpad3":
+      ingreso_por_teclado(3);
+      break;
+    case "Digit3":
+      ingreso_por_teclado(3);
+      break;
+    case "Numpad4":
+      ingreso_por_teclado(4);
+      break;
+    case "Digit4":
+      ingreso_por_teclado(4);
+      break;
+    case "Numpad5":
+      ingreso_por_teclado(5);
+      break;
+    case "Digit5":
+      ingreso_por_teclado(5);
+      break;
+    case "Numpad6":
+      ingreso_por_teclado(6);
+      break;
+    case "Digit6":
+      ingreso_por_teclado(6);
+      break;
+    case "Numpad7":
+      ingreso_por_teclado(7);
+      break;
+    case "Digit7":
+      ingreso_por_teclado(7);
+      break;
+    case "Numpad8":
+      ingreso_por_teclado(8);
+      break;
+    case "Digit8":
+      ingreso_por_teclado(8);
+      break;
+    case "Numpad9":
+      ingreso_por_teclado(9);
+      break;
+    case "Digit9":
+      ingreso_por_teclado(9);
+      break;
+    case "Numpad0":
+      ingreso_por_teclado(0);
+      break;
+    case "Digit0":
+      ingreso_por_teclado(0);
+      break;
+    case "NumpadAdd":
+      tecla_suma();
+      break;
+    case "NumpadSubtract":
+      tecla_resta();
+      break;
+    case "NumpadMultiply":
+      tecla_multiplicacion();
+      break;
+    case "NumpadDivide":
+      tecla_division();
+      break;
+    case "NumpadEnter":
+      tecla_igual();
+      break;
+    case "Enter":
+      tecla_igual();
+      break;
+    case "NumpadDecimal":
+      tecla_punto();
+      break;
+    case "Period":
+      tecla_punto();
+      break;
+    case "Comma":
+      tecla_punto();
+      break;
+    case "Delete":
+      resetear_pantalla();
+      break;
+    default:
+      break;
+  }
+}
+
+function ingreso_por_teclado(x){
+  var estado_anterior = display.innerHTML;
+  if (c.comprobar_decimales(estado_anterior)) {
+    if (estado_anterior == "0") {
+      display.innerHTML = x;
+    } else {
+      display.innerHTML = estado_anterior + x;
+    }
+  } else {
+    alert("Alerta: Máximo 8 decimales") 
+  }
+}
+
 // --- EVENTOS
 // Tecla Numericas
 var teclas_numericas = document.getElementsByClassName('tecla_numero');
 for (var i = 0; i < teclas_numericas.length; i++) {
-  teclas_numericas[i].addEventListener('click', clickAgregarNumero, false);
+  teclas_numericas[i].addEventListener('click', click_agregar_numero);
 }
 
 // Tecla ON/C
-document.getElementById('on').addEventListener('click', resetear_pantalla, false);
+document.getElementById('on').addEventListener('click', resetear_pantalla);
 
 // Tecla Signo
-document.getElementById('sign').addEventListener('click', tecla_signo, false);
+document.getElementById('sign').addEventListener('click', tecla_signo);
 
 // Tecla Punto
-document.getElementById('punto').addEventListener('click', tecla_punto, false);
+document.getElementById('punto').addEventListener('click', tecla_punto);
 
 // Tecla Suma
-document.getElementById('mas').addEventListener('click', tecla_suma, false);
+document.getElementById('mas').addEventListener('click', tecla_suma);
 
 // Tecla Resta
-document.getElementById('menos').addEventListener('click', tecla_resta, false);
+document.getElementById('menos').addEventListener('click', tecla_resta);
 
 // Tecla Multiplicacion
-document.getElementById('por').addEventListener('click', tecla_multiplicacion, false);
+document.getElementById('por').addEventListener('click', tecla_multiplicacion);
 
 // Tecla Division
-document.getElementById('dividido').addEventListener('click', tecla_division, false);
+document.getElementById('dividido').addEventListener('click', tecla_division);
 
 // Tecla Raiz cuadrada
-document.getElementById('raiz').addEventListener('click', tecla_raiz, false);
+document.getElementById('raiz').addEventListener('click', tecla_raiz);
 
 // Tecla Igual
-document.getElementById('igual').addEventListener('click', tecla_igual, false);
+document.getElementById('igual').addEventListener('click', tecla_igual);
+
+// Efectos teclas
+var teclas = document.getElementsByClassName('tecla');
+for (var i = 0; i < teclas.length; i++) {
+  // Efecto Mouse ingresa a tecla
+  teclas[i].addEventListener('mouseover', tecla_raton_ingresa);
+  // Efecto Mouse ingresa a tecla
+  teclas[i].addEventListener('mouseout', tecla_raton_sale);
+  // Efecto Presionar tecla
+  teclas[i].addEventListener('click', tecla_presionar);
+}
+
+// Uso de teclas
+document.addEventListener('keypress', ingresar_tecla);
